63508bc9363fae4c0e486484e565ec5044d8c681
AnuP1306 <anushka789p@gmail.com>
2026-04-09 18:11:53 +0530

    Navbar and left sidebar added

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 
+# create app FIRST
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Server is running 🚀"
+# import routes AFTER creating app
+from routes.home_routes import home_bp
+
+# register routes
+app.register_blueprint(home_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
