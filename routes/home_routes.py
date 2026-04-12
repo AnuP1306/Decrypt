d33782704f8b859e0c98c47165d3628451377f88
samikshashetty31 <samikshashetty53@gmail.com>
2026-04-12 14:04:46 +0530

    WIP: opportunities progress

--- routes/home_routes.py
+++ routes/home_routes.py
@@ -3,5 +3,9 @@ from flask import Blueprint, render_template
 home_bp = Blueprint('home', __name__)
 
 @home_bp.route('/')
+def landing():
+   return render_template("landing.html")
+
+@home_bp.route('/home')
 def home():
-   return render_template("home.html", active_page="home") #added  active_page="home
+   return render_template("home.html", active_page="home")
