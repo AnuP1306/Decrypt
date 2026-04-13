0686ba4f335e11b95bdbc59ad3fbb81a4815a05d
mukepornima-1106 <mukepornima@gmail.com>
2026-04-13 19:09:41 +0530

    Added tools library page with UI, filters and search

--- app.py
+++ app.py
@@ -5,9 +5,11 @@ app = Flask(__name__)
 
 # import routes AFTER creating app
 from routes.home_routes import home_bp
+from routes.tools_routes import tools_bp
 
 # register routes
 app.register_blueprint(home_bp)
+app.register_blueprint(tools_bp) 
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ routes/tools_routes.py
@@ -0,0 +1,7 @@
+from flask import Blueprint, render_template
+
+tools_bp = Blueprint('tools', __name__)
+
+@tools_bp.route('/tools')
+def tools():
+    return render_template('tools.html')
